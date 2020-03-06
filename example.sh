435c4375d6eb1e7afcb29a2d81915b2eb297a9c8
root <root@ip-172-31-89-186.ec2.internal>
2020-03-06 12:24:53 +0000

    linux examples

---
+++ example.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "Hello Welcome to shell scripting"
+echo 'this is a single quote message'
+echo HEy there this is without any quotation
+echo "--------------------------------"
+echo "$(ls)"
+echo $(ls)
---
+++ variable.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+#<< comment
+ls 
+pwd
+#ls -larth
+
+echo "-------------------"
+
+x=3
+y=5
+echo "The value of x: $x"
+echo "The value of y: $y"
+
+echo ###########################################
+echo The value of x: $x
+echo The value of y: $y
+echo $z
+
+z=45
+echo "$z"
+
+#comment
+
+
+
+echo ####################################################################
+
+x=1
+y=2
+z=3
+
+
+echo "------------------------------------------------------"
+echo -e "The value of x: $x \n The value of y: $y \n The value of z: $z"
+
+
+
+
+
+
+
+
+
+
+
+
+
